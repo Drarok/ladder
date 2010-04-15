c1d97c54882cd1d1f277ef463b9a5d57793f0de4
Drarok Ithaqua <ladder@catchall.drarok.com>
2010-04-15 09:56:52 +0000

    Updated the status command.
    --HG--
    extra : convert_revision : svn%3A4fd3d1b2-7acc-2142-88d6-33460400c3cb/ladder/trunk%409029

--- system/core/commands/status.php
+++ system/core/commands/status.php
@@ -1,6 +1,17 @@
 <?php
 
+// Grab all the migrations and sort them.
+$migrations = glob(APPPATH.'migrations'.DS.'*.php');
+sort($migrations);
+
+// Split the numeric part off the last one.
+list($latest_id) = explode('-', basename(end($migrations)), 2);
+
+// Make sure it's an integer;
+$latest_id = (int) $latest_id;
+
 echo "Migration Status\n";
+echo 'Latest migration is ', $latest_id, "\n";
 
 $db = Database::factory();
 
@@ -8,5 +19,24 @@ while ($db->next_database()) {
 	$db->check_migrations_table();
 	$res = $db->query('SELECT MAX(`migration`) AS `max_migration` FROM `migrations`');
 	$row = mysql_fetch_object($res);
-	echo "\t", $db->name, ': ', (int) $row->max_migration, "\n";
+
+	// Grab the latest migration as an integer.
+	$max_migration = (int) $row->max_migration;
+
+	// Work out the status identifier.
+	if ($latest_id === $max_migration) {
+		// This database is up-to-date.
+		$status = '=';
+	} elseif ($latest_id > $max_migration) {
+		// This database is out-of-date.
+		$status = '<';
+	} elseif ($latest_id < $max_migration) {
+		// This database is more recent than the latest!
+		$status = '!';
+	} else {
+		// This should be impossible to reach.
+		$status = 'E';
+	}
+
+	echo $status, "\t", $db->name, ': ', $max_migration, "\n";
 }
