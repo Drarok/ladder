1b01fbbb785e63f9b84e14c20dcff1531a73e378
Drarok Ithaqua <ladder@catchall.drarok.com>
2010-04-19 14:10:58 +0000

    Document which commands accept --force.
    --HG--
    extra : convert_revision : svn%3A4fd3d1b2-7acc-2142-88d6-33460400c3cb/ladder/trunk%409081

--- system/core/bootstrap.php
+++ system/core/bootstrap.php
@@ -92,6 +92,6 @@ Usage: php ladder.php <command> [options]
 	--run-tests               - run any test() methods in the migrations that are processed.
 	--show-sql                - output any sql queries before they are executed.
 	--verbose                 - include extra output (only used by "status" at present).
-	--force                   - force operations to run that normally wouldn't.
+	--force                   - force operations to run (applies to 'add', 'remove', and 'reapply').
 <?php
 };
