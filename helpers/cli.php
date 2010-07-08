98df85f6f85929e98a90ade8a822e4abd91f695d
Drarok Ithaqua <ladder@catchall.drarok.com>
2010-07-08 17:35:00 +0000

    Extend the cli helper to parse --options.
    --HG--
    branch : kohana_module
    extra : convert_revision : svn%3A4fd3d1b2-7acc-2142-88d6-33460400c3cb/ladder/branches/kohana_module%4010345

--- helpers/cli.php
+++ helpers/cli.php
@@ -4,14 +4,12 @@ abstract class cli {
 	protected static $command;
 	
 	/**
-	 * Return the first non-option argument.
+	 * Return the nth non-option argument.
 	 */
-	public static function command() {
-		if ((bool) self::$command) {
-			return self::$command;
-		}
+	public static function argument($index) {
+		$count = 0;
 		
-		foreach (array_slice($_SERVER['argv'], 2) as $index => $arg) {
+		foreach (array_slice($_SERVER['argv'], 2) as $arg) {
 			// No leading or trailing whitespace.
 			if (! (bool) $arg = trim($arg)) {
 				continue;
@@ -22,10 +20,56 @@ abstract class cli {
 				continue;
 			}
 			
-			// If we got this far, we have a winner.
-			return self::$command = $arg;
+			// If we got this far, we have a valid argument.
+			if ($count == $index) {
+				return $arg;
+			} else {
+				$count++;
+			}
 		}
 		
-		return self::$command = 'help';
+		return FALSE;
+	}
+	
+	/**
+	 * Return the 'command' - the 1st non-option argument.
+	 */
+	public static function command() {
+		if (! (bool) self::$command) {
+			self::$command = self::argument(0);
+		}
+		
+		return self::$command;
+		
+	}
+	
+	/**
+	 * Parse the '--option' and '--option=value' arguments.
+	 */
+	public static function option($name, $default = FALSE) {
+		foreach (array_slice($_SERVER['argv'], 2) as $arg) {
+			// Ignore blank arguments.
+			if (! (bool) $arg = trim($arg)) {
+				continue;
+			}
+			
+			// If it's not an option argument, skip it.
+			if (substr($arg, 0, 2) != '--') {
+				continue;
+			}
+			
+			// It's a boolean TRUE if no '=' part.
+			if (strpos($arg, '=') === FALSE) {
+				$arg_name = substr($arg, 2);
+				$arg_value = TRUE;
+			} else {
+				// Split the '--command=value' into 'command' and 'value'.
+				list($arg_name, $arg_value) = explode('=', substr($arg, 2), 2);
+			}
+			
+			if (strtolower($arg_name) == strtolower($name)) {
+				return $arg_value;
+			}
+		}
 	}
 }
